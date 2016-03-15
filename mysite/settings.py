291f14d1fdce5b8fbf500a9c08e09142f22ccf3b
jlarson497 <kf2617gi@go.minneapolis.edu>
2016-03-15 14:56:40 -0500

    new commit

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 's59een2vbhznl2c5$_97&0r@m%h)nux($u1h6a0ie3x-)(z^^n'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
